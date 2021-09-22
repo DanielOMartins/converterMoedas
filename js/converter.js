ba7c6d63143b1ec3eb488893fe09d2ad70e3d6d8
Daniel <danielomartins.16@gmail.com>
2021-09-22 16:10:11 -0300

    Funções funcionando

---
+++ js/converter.js
@@ -0,0 +1,33 @@
+const converterMoeda = (evento) =>{
+    evento.preventDefault();
+
+    var moeda = document.querySelector('[data-moeda]');
+    moeda = moeda.value + '-BRL';
+
+    const url = `https://economia.awesomeapi.com.br/json/last/${moeda}`;
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        Headers: {
+            'content-type':'application/json;charset=utf-8'
+        }
+    }
+
+    fetch(url, options).then(
+        response => response.json()
+    ).then(
+        data =>{
+            sessionStorage.setItem('resultado', converterValor(data[Object.keys(data)[0]]));
+        }
+    )
+    window.location.href = './resultado.html';
+}
+const converterBtn = document.querySelector('[data-converter]');
+converterBtn.addEventListener('click', converterMoeda);
+
+function converterValor(data){
+    const valor = parseFloat(document.querySelector('[data-valor]').value);
+    const resultado = valor * data.ask;
+
+    return parseFloat(resultado);
+}
